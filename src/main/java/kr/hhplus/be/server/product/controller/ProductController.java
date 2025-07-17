6bddfabbf434fbed810e6674e1013dba682e1e7f
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2025-07-18 05:06:40 +0900

    feat: 상품 관련 API Mock API 및 Swagger 작성

--- src/main/java/kr/hhplus/be/server/product/controller/ProductController.java
+++ src/main/java/kr/hhplus/be/server/product/controller/ProductController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ public class ProductController {
 
     @GetMapping("/{productId}")
     @Operation(summary = "상품 조회", description = "특정 상품을 조회합니다.")
-    public ResponseEntity<?> getProduct() {
+    public ResponseEntity<?> getProduct(@PathVariable int productId) {
         return ResponseEntity.ok().build();
     }
 

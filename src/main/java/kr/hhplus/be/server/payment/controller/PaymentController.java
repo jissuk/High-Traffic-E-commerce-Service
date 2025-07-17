6aa1171b570bc019ca4b3d85726a8caaa2290c12
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2025-07-18 04:46:01 +0900

    feat: 결제 관련 API Mock API 및 Swagger 작성

---
+++ src/main/java/kr/hhplus/be/server/payment/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package kr.hhplus.be.server.payment.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payments")
+@Tag(name = "payment", description = "결제 관련 API")
+public class PaymentController {
+
+    @PostMapping
+    @Operation(summary = "결제 요청", description = "유저는 취소되지 않은 주문에 대해 포인트를 사용하여 결제를 요청할 수 있습니다.")
+    public ResponseEntity<?> requestPayment() {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{paymentId}")
+    @Operation(summary = "결제 조회", description = "유저는 주문의 결제 상태를 조회할 수 있습니다.")
+    public ResponseEntity<?> getPaymentById(){
+        return ResponseEntity.ok().build();
+    }
+}

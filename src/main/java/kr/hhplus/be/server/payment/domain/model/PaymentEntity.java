3771b629fb35b995ed402366036a7591b33bfaad
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2025-07-31 18:52:11 +0900

    feat: 결제 도메인의 JpaRepository 구현 및 RepositoryImpl 구현 코드 작성

--- src/main/java/kr/hhplus/be/server/payment/domain/model/PaymentEntity.java
+++ src/main/java/kr/hhplus/be/server/payment/domain/model/PaymentEntity.java
@@ -1,5 +1,6 @@
 package kr.hhplus.be.server.payment.domain.model;
 
+import jakarta.persistence.*;
 import kr.hhplus.be.server.coupon.domain.model.CouponEntity;
 import kr.hhplus.be.server.order.domain.model.OrderItemEntity;
 import kr.hhplus.be.server.user.domain.model.UserEntity;
@@ -7,17 +8,35 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "TBL_PAYMENT")
 @Getter
 @Setter
 @Builder
 public class PaymentEntity {
-    long id;
-    long price;
-    PaymentStatus paymentStatus;
-    LocalDateTime createAt;
 
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private long price;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+    @Column
+    private LocalDateTime createAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     UserEntity user;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "coupon_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     CouponEntity coupon;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_item_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     OrderItemEntity orderItem;
 
     public PaymentEntity() {
@@ -33,16 +52,4 @@ public class PaymentEntity {
         this.orderItem = orderItem;
     }
 
-    @Override
-    public String toString() {
-        return "PaymentEntity{" +
-                "id=" + id +
-                ", price=" + price +
-                ", paymentStatus=" + paymentStatus +
-                ", createAt=" + createAt +
-                ", user=" + user +
-                ", coupon=" + coupon +
-                ", orderItem=" + orderItem +
-                '}';
-    }
 }

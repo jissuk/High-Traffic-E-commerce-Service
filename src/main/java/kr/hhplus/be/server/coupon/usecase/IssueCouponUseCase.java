528b7d80615b6bd53f845d3816dca030d6f6b89f
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2025-08-15 02:43:03 +0900

    refactor: 객체 생성 패턴 변경으로 Setter를 사용하지 않고 참조키를 주입

--- src/main/java/kr/hhplus/be/server/coupon/usecase/IssueCouponUseCase.java
+++ src/main/java/kr/hhplus/be/server/coupon/usecase/IssueCouponUseCase.java
@@ -1,21 +1,19 @@
 package kr.hhplus.be.server.coupon.usecase;
 
+import kr.hhplus.be.server.common.annotation.DistributedLock;
 import kr.hhplus.be.server.common.annotation.UseCase;
 import kr.hhplus.be.server.coupon.domain.model.Coupon;
 import kr.hhplus.be.server.coupon.domain.model.UserCoupon;
 import kr.hhplus.be.server.coupon.domain.repository.CouponRepository;
 import kr.hhplus.be.server.coupon.domain.repository.UserCouponRepository;
-import kr.hhplus.be.server.coupon.exception.CouponNotFoundException;
 import kr.hhplus.be.server.coupon.usecase.command.UserCouponCommand;
 import kr.hhplus.be.server.user.domain.model.User;
 import kr.hhplus.be.server.user.domain.repository.UserRepository;
-import kr.hhplus.be.server.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
 
 @UseCase
-@Transactional
 @RequiredArgsConstructor
 public class IssueCouponUseCase {
 
@@ -23,20 +21,17 @@ public class IssueCouponUseCase {
     private final CouponRepository couponRepository;
     private final UserCouponRepository userCouponRepository;
 
+    @DistributedLock(key = "'coupon:issue:' + #command.userId")
     @Transactional
     public void execute(UserCouponCommand command) {
 
-        Coupon coupon = couponRepository.findByIdForUpdate(command.couponId());
-
-
+        Coupon coupon = couponRepository.findById(command.couponId());
         User user = userRepository.findById(command.userId());
 
         coupon.checkQuantity();
         coupon.decreaseQuantity();
 
-        UserCoupon userCoupon = UserCoupon.createBeforeUserCoupon(coupon);
-        userCoupon.setCouponId(user.getId());
-        userCoupon.setCouponId(coupon.getId());
+        UserCoupon userCoupon = UserCoupon.createBeforeUserCoupon(coupon, user);
 
         couponRepository.save(coupon);
         userCouponRepository.save(userCoupon);

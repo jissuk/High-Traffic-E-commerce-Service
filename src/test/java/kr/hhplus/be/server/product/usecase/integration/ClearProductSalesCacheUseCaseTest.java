6c86adb812aa8348342d3fdd7c4995db73617898
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2025-08-15 02:27:05 +0900

    test: 인기상품조회에 필요한 캐시를 삭제하는 테스트 코드 작성

---
+++ src/test/java/kr/hhplus/be/server/product/usecase/integration/ClearProductSalesCacheUseCaseTest.java
@@ -0,0 +1,44 @@
+package kr.hhplus.be.server.product.usecase.integration;
+
+
+import kr.hhplus.be.server.product.usecase.ClearProductSalesCacheUseCase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.testcontainers.utility.TestcontainersConfiguration;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Import(TestcontainersConfiguration.class)
+@DisplayName("상품 관련 테스트")
+public class ClearProductSalesCacheUseCaseTest {
+    
+    @Autowired
+    private ClearProductSalesCacheUseCase clearProductSalesCacheUseCase;
+
+    @Autowired
+    private RedisTemplate<String, Object> redis;
+
+    @Test
+    void 인기상품캐시삭제() {
+
+        // when
+        clearProductSalesCacheUseCase.execute();
+
+        // then
+        String hashKey = "PopularProduct";
+        String zsetKey = "sales:" + LocalDate.now().minusDays(4);
+
+        Boolean productExists = redis.hasKey(hashKey);
+        Boolean salesExists = redis.hasKey(zsetKey);
+
+        assertThat(productExists).isFalse();
+        assertThat(salesExists).isFalse();
+    }
+}

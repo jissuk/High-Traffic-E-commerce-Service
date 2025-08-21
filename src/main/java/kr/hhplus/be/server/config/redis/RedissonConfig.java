eb9c9961794883030644d2e0c14e878015267850
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2025-08-21 23:24:47 +0900

    refactor: Redis 설정 방식 변경

--- src/main/java/kr/hhplus/be/server/config/redis/RedissonConfig.java
+++ src/main/java/kr/hhplus/be/server/config/redis/RedissonConfig.java
@@ -1,27 +1,23 @@
 package kr.hhplus.be.server.config.redis;
 
+import lombok.RequiredArgsConstructor;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class RedissonConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String host;
+    private final RedisProperties properties;
 
-    @Value("${spring.data.redis.port}")
-    private int port;
-
-    // RedissonClient (분산 락)
     @Bean(destroyMethod = "shutdown")
     public RedissonClient redissonClient() {
         Config config = new Config();
         config.useSingleServer()
-                .setAddress("redis://" + host + ":" + port);
+                .setAddress("redis://" + properties.getHost() + ":" + properties.getPort());
         return Redisson.create(config);
     }
 }

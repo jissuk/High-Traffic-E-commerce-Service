6c1467997e84b275eb19ed793af32ca021f13b26
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2025-08-27 23:57:50 +0900

    refactor: Command에서 LockKeyProvider를 구현

--- src/main/java/kr/hhplus/be/server/coupon/usecase/command/UserCouponCommand.java
+++ src/main/java/kr/hhplus/be/server/coupon/usecase/command/UserCouponCommand.java
@@ -1,15 +1,21 @@
 package kr.hhplus.be.server.coupon.usecase.command;
 
+import kr.hhplus.be.server.common.provider.LockKeyProvider;
 import kr.hhplus.be.server.coupon.usecase.dto.UserCouponRequest;
 import lombok.Builder;
 
 @Builder
-public record UserCouponCommand(Long userId, Long couponId) {
+public record UserCouponCommand(Long userId, Long couponId) implements LockKeyProvider {
 
     public static UserCouponCommand from(UserCouponRequest dto){
         return UserCouponCommand.builder()
                 .userId(dto.userId())
                 .couponId(dto.couponId())
                 .build();
+    }
+
+    @Override
+    public String lockKey() {
+        return "coupon:issue:" + userId + ":lock";
     }
 }

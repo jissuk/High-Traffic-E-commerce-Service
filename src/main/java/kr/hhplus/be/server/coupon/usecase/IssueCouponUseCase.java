7fd3fa80115d05edb4b453f8b295e6337391bd66
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2025-08-28 00:46:44 +0900

    refactor: RedisKey 리펙토링 및 이벤트 로직으로 인한 코드 수정

--- src/main/java/kr/hhplus/be/server/coupon/usecase/IssueCouponUseCase.java
+++ src/main/java/kr/hhplus/be/server/coupon/usecase/IssueCouponUseCase.java
@@ -2,12 +2,15 @@ package kr.hhplus.be.server.coupon.usecase;
 
 import kr.hhplus.be.server.common.annotation.DistributedLock;
 import kr.hhplus.be.server.common.annotation.UseCase;
+import kr.hhplus.be.server.coupon.event.IssueCouponEvent;
 import kr.hhplus.be.server.coupon.exception.CouponOutOfStockException;
 import kr.hhplus.be.server.coupon.exception.DuplicateCouponIssueException;
 import kr.hhplus.be.server.coupon.usecase.command.UserCouponCommand;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,22 +20,22 @@ import java.util.concurrent.TimeUnit;
 public class IssueCouponUseCase {
 
     private final RedisTemplate<String, Long> redis;
-    private final StringRedisTemplate couponRedis;
+
+    private final TransactionTemplate transactionTemplate;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     public static final String COUPON_ISSUE_PREFIX = "coupon:issue:";
     public static final String ISSUED_SUFFIX = ":issued";
     public static final String QUANTITY_SUFFIX = ":quantity";
-    public static final String COUPON_ISSUE_QUEUE = "coupon:issue:queue";
 
-    public static final long COUPON_REQUEST_EXPIRE_MINUTES = 5;
-
-    @DistributedLock(key = "T(kr.hhplus.be.server.common.constant.RedisKey.Coupon).LOCK_COUPON_ISSUE + #command.userId + ':lock'")
+    @DistributedLock
     public void execute(UserCouponCommand command) {
-        validateDuplicateIssue(command);
-        decrementQuantity(command);
-        queueCouponIssue(command);
+        transactionTemplate.executeWithoutResult(status -> {
+            validateDuplicateIssue(command);
+            decrementQuantity(command);
+            applicationEventPublisher.publishEvent(new IssueCouponEvent(command.userId(), command.couponId()));
+        });
     }
-
 
     private void validateDuplicateIssue(UserCouponCommand command) {
         String issuedKey = COUPON_ISSUE_PREFIX + command.userId() + ISSUED_SUFFIX;
@@ -46,19 +49,11 @@ public class IssueCouponUseCase {
     }
 
     private void decrementQuantity(UserCouponCommand command) {
-        String quantityKey = ISSUED_SUFFIX + command.couponId() + QUANTITY_SUFFIX;
-
+        String quantityKey = COUPON_ISSUE_PREFIX + command.couponId() + QUANTITY_SUFFIX;
         Long quantity = redis.opsForValue().decrement(quantityKey);
-
         if(quantity == null || quantity < 0){
             throw new CouponOutOfStockException();
         }
     }
 
-    private void queueCouponIssue(UserCouponCommand command) {
-        String value = command.userId() + ":" + command.couponId();
-
-        couponRedis.opsForZSet().add(COUPON_ISSUE_QUEUE, value,System.currentTimeMillis());
-        couponRedis.expire(COUPON_ISSUE_QUEUE, COUPON_REQUEST_EXPIRE_MINUTES, TimeUnit.MINUTES);
-    }
 }

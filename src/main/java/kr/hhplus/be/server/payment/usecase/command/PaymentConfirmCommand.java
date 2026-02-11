06d1d50e21eecfa9898f39139faad23667b495b0
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2026-02-12 00:09:08 +0900

    feat: Kafka 설정 파일 추가

--- src/main/java/kr/hhplus/be/server/payment/usecase/command/PaymentConfirmCommand.java
+++ src/main/java/kr/hhplus/be/server/payment/usecase/command/PaymentConfirmCommand.java
@@ -4,9 +4,10 @@ import kr.hhplus.be.server.payment.usecase.dto.PaymentConfirmRequest;
 import lombok.Builder;
 
 @Builder
-public record PaymentConfirmCommand(String orderId, String paymentKey, Long amount) {
+public record PaymentConfirmCommand(Long paymentId, String orderId, String paymentKey, Long amount) {
     public static PaymentConfirmCommand from(PaymentConfirmRequest request) {
         return  PaymentConfirmCommand.builder()
+                                        .paymentId(request.paymentId())
                                         .orderId(request.orderId())
                                         .paymentKey(request.paymentKey())
                                         .amount(request.amount())

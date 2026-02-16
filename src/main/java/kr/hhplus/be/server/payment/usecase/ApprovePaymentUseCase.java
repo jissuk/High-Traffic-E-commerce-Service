51a9e75004f916835498f191909610fa3fd33fcd
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2026-02-16 15:40:41 +0900

    refactor: 불필요한 클래스 삭제

--- src/main/java/kr/hhplus/be/server/payment/usecase/ApprovePaymentUseCase.java
+++
@@ -1,9 +0,0 @@
-package kr.hhplus.be.server.payment.usecase;
-
-import kr.hhplus.be.server.payment.usecase.command.PaymentConfirmCommand;
-
-public class ApprovePaymentUseCase {
-    public void execute(PaymentConfirmCommand command) {
-
-    }
-}

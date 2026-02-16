b1f3eb9b2ded88cdca6dd3ad4b7afd6731f1313d
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2026-02-16 13:49:50 +0900

    refactor: 결제 상태 값 추가

--- src/main/java/kr/hhplus/be/server/payment/domain/model/PaymentStatus.java
+++ src/main/java/kr/hhplus/be/server/payment/domain/model/PaymentStatus.java
@@ -1,7 +1,8 @@
 package kr.hhplus.be.server.payment.domain.model;
 
 public enum PaymentStatus {
-    BEFORE_PAYMENT,    // 결제 전
+    PENDING,           // 결제 대기
+    REQUESTED,         // 결제 요청
     APPROVED,          // 결제 승인 완료
     CANCELED           // 결제 취소
 }

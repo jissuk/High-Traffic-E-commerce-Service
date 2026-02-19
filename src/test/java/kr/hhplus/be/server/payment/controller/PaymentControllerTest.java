6ee79d4ae6cf0ba4c563b4bf4bb207a5fff2344d
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2026-02-19 23:36:57 +0900

    refactor: 결제 도메인의 테스트 코드 리펙토링

--- src/test/java/kr/hhplus/be/server/payment/controller/PaymentControllerTest.java
+++ src/test/java/kr/hhplus/be/server/payment/controller/PaymentControllerTest.java
@@ -1,190 +1,109 @@
-//package kr.hhplus.be.server.payment.controller;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import kr.hhplus.be.server.coupon.domain.model.CouponEntity;
-//import kr.hhplus.be.server.coupon.infrastructure.jpa.JpaCouponRepository;
-//import kr.hhplus.be.server.coupon.infrastructure.jpa.JpaUserCouponRepository;
-//import kr.hhplus.be.server.coupon.step.CouponStep;
-//import kr.hhplus.be.server.order.domain.model.OrderEntity;
-//import kr.hhplus.be.server.order.domain.model.OrderItemEntity;
-//import kr.hhplus.be.server.order.infrastructure.jpa.JpaOrderItemRepository;
-//import kr.hhplus.be.server.order.infrastructure.jpa.JpaOrderRepository;
-//import kr.hhplus.be.server.order.step.OrderStep;
-//import kr.hhplus.be.server.payment.infrastructure.jpa.JpaPaymentRepository;
-//import kr.hhplus.be.server.payment.step.PaymentStep;
-//import kr.hhplus.be.server.product.infrastructure.jpa.JpaProductRepository;
-//import kr.hhplus.be.server.product.step.ProductStep;
-//import kr.hhplus.be.server.user.domain.model.UserEntity;
-//import kr.hhplus.be.server.user.infrastructure.jpa.JpaUserRepository;
-//import kr.hhplus.be.server.user.step.UserStep;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Nested;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.context.annotation.Import;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.ResultActions;
-//import org.testcontainers.utility.TestcontainersConfiguration;
-//
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-//
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-//@AutoConfigureMockMvc
-//@Import(TestcontainersConfiguration.class)
-//@DisplayName("결제 관련 테스트")
-//public class PaymentControllerTest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Autowired
-//    private ObjectMapper objectMapper;
-//
-//    @Autowired
-//    private JdbcTemplate jdbcTemplate;
-//
-//    @Autowired
-//    private JpaUserRepository jpaUserRepository;
-//    @Autowired
-//    private JpaProductRepository jpaProductRepository;
-//    @Autowired
-//    private JpaOrderItemRepository jpaOrderItemRepository;
-//    @Autowired
-//    private JpaOrderRepository jpaOrderRepositroy;
-//    @Autowired
-//    private JpaPaymentRepository jpaPaymentRepository;
-//    @Autowired
-//    private JpaCouponRepository jpaCouponRepository;
-//    @Autowired
-//    private JpaUserCouponRepository jpaUserCouponRepository;
-//
-//    @BeforeEach
-//    void setUp() {
-//        clearTestData();
-//        initTestData();
-//    }
-//    private void clearTestData() {
-//        jdbcTemplate.execute("TRUNCATE TABLE users;");
-//        jdbcTemplate.execute("TRUNCATE TABLE orders;");
-//        jdbcTemplate.execute("TRUNCATE TABLE order_items;");
-//        jdbcTemplate.execute("TRUNCATE TABLE payments;");
-//        jdbcTemplate.execute("TRUNCATE TABLE coupons;");
-//        jdbcTemplate.execute("TRUNCATE TABLE user_coupons;");
-//        jdbcTemplate.execute("TRUNCATE TABLE products;");
-//    }
-//
-//    private void initTestData() {
-//        UserEntity user = jpaUserRepository.save(UserStep.defualtUserEntity());
-//        CouponEntity coupon = jpaCouponRepository.save(CouponStep.defaultCouponEntity());
-//        OrderEntity order = jpaOrderRepositroy.save(OrderStep.defaultOrderEntity(user));
-//        OrderItemEntity orderItem = jpaOrderItemRepository.save(OrderStep.defaultOrderItemEntity(order));
-//
-//        jpaUserCouponRepository.save(CouponStep.defaultUserCouponEntity(user, coupon));
-//        jpaProductRepository.save(ProductStep.defaultProductEntity());
-//        jpaPaymentRepository.save(PaymentStep.defaultPaymentEntity(user,orderItem));
-//    }
-//
-//    @Nested
-//    @DisplayName("결제 성공 케이스")
-//    class success {
-//
-//        @Test
-//        @DisplayName("요청 데이터가 정상적일 경우 쿠폰과 포인트를 사용하여 결제한다.")
-//        void 결제() throws Exception {
-//            // givne
-//            PaymentRequestRequest request = PaymentStep.defaultPaymentRequest();
-//
-//            // when
-//            ResultActions result = PaymentStep.paymentRequest(mockMvc, objectMapper, request);
-//
-//            // then
-//            result.andExpect(status().isOk());
-//        }
-//
-//        @Test
-//        @DisplayName("요청 데이터가 정상적일 경우 포인트만 사용하여 결제한다.")
-//        void 결제_쿠폰이존재하지않을_경우() throws Exception {
-//            // givne
-//            PaymentRequestRequest request = PaymentStep.paymentRequestWithCouponId(null);
-//
-//            // when
-//            ResultActions result = PaymentStep.paymentRequest(mockMvc, objectMapper, request);
-//
-//            // then
-//            result.andExpect(status().isOk());
-//        }
-//    }
-//
-//    @Nested
-//    @DisplayName("결제 실패 케이스")
-//    class fail{
-//        @Test
-//        @DisplayName("존재하지 않는 유저일 경우 UserNotFoundException이 발생한다.")
-//        void 결제_존재하지않는_유저일_경우() throws Exception {
-//            // givne
-//            PaymentRequestRequest request = PaymentStep.paymentRequestWithUserId(2L);
-//
-//            // when
-//            ResultActions result = PaymentStep.paymentRequest(mockMvc, objectMapper, request);
-//
-//            // then
-//            result.andExpect(jsonPath("$.code").value("UserNotFound"));
-//        }
-//
-//        @Test
-//        @DisplayName("존재하지 않는 주문일 경우 OrderNotFoundException이 발생한다.")
-//        void 결제_존재하지않는_주문일_경우() throws Exception {
-//            // givne
-//            PaymentRequestRequest request = PaymentStep.paymentRequestWithOrderId(2L);
-//
-//            // when
-//            ResultActions result = PaymentStep.paymentRequest(mockMvc, objectMapper, request);
-//
-//            // then
-//            result.andExpect(jsonPath("$.code").value("OrderNotFound"));
-//        }
-//
-//        @Test
-//        @DisplayName("존재하지 않는 주문상세일 경우 OrderItemNotFoundException이 발생한다.")
-//        void 결제_존재하지않는_주문상세일_경우() throws Exception {
-//            // givne
-//            PaymentRequestRequest request = PaymentStep.paymentRequestWithOrderItemId(2L);
-//
-//            // when
-//            ResultActions result = PaymentStep.paymentRequest(mockMvc, objectMapper, request);
-//
-//            // then
-//            result.andExpect(jsonPath("$.code").value("OrderItemNotFound"));
-//        }
-//
-//        @Test
-//        @DisplayName("존재하지 않는 결제일 경우 PaymentNotFoundException이 발생한다.")
-//        void 결제_존재하지않는_결제일_경우() throws Exception {
-//            // givne
-//            PaymentRequestRequest request = PaymentStep.paymentRequestWithPaymentId(2L);
-//
-//            // when
-//            ResultActions result = PaymentStep.paymentRequest(mockMvc, objectMapper, request);
-//
-//            // then
-//            result.andExpect(jsonPath("$.code").value("PaymentNotFound"));
-//        }
-//
-//        @Test
-//        @DisplayName("존재하지 않는 상품일 경우 ProductNotFoundException이 발생한다.")
-//        void 결제_존재하지않는_상품일_경우() throws Exception {
-//            // givne
-//            PaymentRequestRequest request = PaymentStep.paymentRequestWithProductId(2L);
-//
-//            // when
-//            ResultActions result = PaymentStep.paymentRequest(mockMvc, objectMapper, request);
-//
-//            // then
-//            result.andExpect(jsonPath("$.code").value("ProductNotFound"));
-//        }
-//    }
-//}
+package kr.hhplus.be.server.payment.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.hhplus.be.server.order.domain.model.Order;
+import kr.hhplus.be.server.order.domain.model.OrderItem;
+import kr.hhplus.be.server.order.infrastructure.jpa.JpaOrderItemRepository;
+import kr.hhplus.be.server.order.infrastructure.jpa.JpaOrderRepository;
+import kr.hhplus.be.server.order.step.OrderStep;
+import kr.hhplus.be.server.payment.domain.model.Payment;
+import kr.hhplus.be.server.payment.infrastructure.jpa.JpaPaymentRepository;
+import kr.hhplus.be.server.payment.step.PaymentStep;
+import kr.hhplus.be.server.payment.usecase.dto.PaymentRequest;
+import kr.hhplus.be.server.user.domain.model.User;
+import kr.hhplus.be.server.user.infrastructure.jpa.JpaUserRepository;
+import kr.hhplus.be.server.user.step.UserStep;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.testcontainers.utility.TestcontainersConfiguration;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@Import(TestcontainersConfiguration.class)
+@DisplayName("결제 관련 테스트")
+public class PaymentControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private JpaUserRepository jpaUserRepository;
+    @Autowired
+    private JpaOrderRepository jpaOrderRepository;
+    @Autowired
+    private JpaOrderItemRepository jpaOrderItemRepository;
+    @Autowired
+    private JpaPaymentRepository jpaPaymentRepository;
+
+    @BeforeEach
+    void setUp() {
+        clearTestData();
+        initTestData();
+    }
+    private void clearTestData() {
+        jdbcTemplate.execute("TRUNCATE TABLE users;");
+        jdbcTemplate.execute("TRUNCATE TABLE orders;");
+        jdbcTemplate.execute("TRUNCATE TABLE order_items;");
+        jdbcTemplate.execute("TRUNCATE TABLE payments;");
+    }
+
+    private void initTestData() {
+        User user = UserStep.defualtUser();
+        Order order = OrderStep.orderWithUserId(user);
+        OrderItem orderItem = OrderStep.defaultOrderItem(order);
+        Payment payment = PaymentStep.paymentWithOrderId(order);
+
+        jpaUserRepository.save(user);
+        jpaOrderRepository.save(order);
+        jpaOrderItemRepository.save(orderItem);
+        jpaPaymentRepository.save(payment);
+    }
+
+    @Nested
+    @DisplayName("결제 성공 케이스")
+    class success {
+        @Test
+        @DisplayName("요청 데이터가 정상적일 경우 쿠폰과 포인트를 사용하여 결제한다.")
+        void 결제() throws Exception {
+            // givne
+            PaymentRequest request = PaymentStep.defaultPaymentRequest();
+
+            // when
+            ResultActions result = PaymentStep.paymentRequest(mockMvc, objectMapper, request);
+
+            // then
+            result.andExpect(status().isOk());
+        }
+    }
+
+    @Nested
+    @DisplayName("결제 실패 케이스")
+    class fail{
+        @Test
+        @DisplayName("존재하지 않는 주문일 경우 OrderNotFoundException이 발생한다.")
+        void 결제_존재하지않는_주문일_경우() throws Exception {
+            // givne
+            long orderId = 2L;
+            PaymentRequest request = PaymentStep.paymentRequestWithOrderId(orderId);
+
+            // when
+            ResultActions result = PaymentStep.paymentRequest(mockMvc, objectMapper, request);
+
+            // then
+            result.andExpect(jsonPath("$.code").value("OrderNotFound"));
+        }
+    }
+}
--- src/test/java/kr/hhplus/be/server/payment/step/PaymentStep.java
+++ src/test/java/kr/hhplus/be/server/payment/step/PaymentStep.java
@@ -1,126 +1,74 @@
-//package kr.hhplus.be.server.payment.step;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import kr.hhplus.be.server.order.domain.model.OrderItemEntity;
-//import kr.hhplus.be.server.payment.domain.model.PaymentEntity;
-//import kr.hhplus.be.server.payment.domain.model.PaymentStatus;
-//import kr.hhplus.be.server.payment.usecase.command.PaymentCommand;
-//import kr.hhplus.be.server.user.domain.model.UserEntity;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.ResultActions;
-//
-//
-//import java.time.LocalDateTime;
-//import java.time.ZoneId;
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-//
-//public class PaymentStep {
-//
-//    private static String PATH_URL = "/payments";
-//
-//    public static PaymentCommand defaultPaymentCommand() {
-//        return new PaymentCommand(1L, 1L, 1L, 1L, 1L);
-//    }
-//
-//    public static PaymentEntity paymentEntityWithOrderItemIdAndCreateAt(long id, LocalDateTime createdAt) {
-//        return PaymentEntity.builder()
-//                .price(3000L)
-//                .paymentStatus(PaymentStatus.COMPLETED)
-//                .orderItemId(id)
-//                .createAt(createdAt)
-//                .build();
-//    }
-//
-//    public static PaymentEntity defaultPaymentEntity(UserEntity user, OrderItemEntity orderItem) {
-//        return PaymentEntity.builder()
-//                .price(3000L)
-//                .paymentStatus(PaymentStatus.PENDING)
-//                .createAt(LocalDateTime.now(ZoneId.of("Asia/Seoul")))
-//                .userId(user.getId())
-//                .orderItemId(orderItem.getId())
-//                .build();
-//    }
-//
-//    public static PaymentRequestRequest defaultPaymentRequest(){
-//        return PaymentRequestRequest.builder()
-//                                .userId(1L)
-//                                .orderId(1L)
-//                                .orderItemId(1L)
-//                                .couponId(1L)
-//                                .productId(1L)
-//                                .build();
-//    }
-//
-//    public static PaymentRequestRequest paymentRequestWithCouponId(Long couponId){
-//        return PaymentRequestRequest.builder()
-//                                .userId(1L)
-//                                .orderId(1L)
-//                                .orderItemId(1L)
-//                                .couponId(couponId)
-//                                .productId(1L)
-//                                .build();
-//    }
-//
-//    public static PaymentRequestRequest paymentRequestWithUserId(long userId){
-//        return PaymentRequestRequest.builder()
-//                .userId(userId)
-//                .orderId(1L)
-//                .orderItemId(1L)
-//                .couponId(1L)
-//                .productId(1L)
-//                .build();
-//    }
-//
-//    public static PaymentRequestRequest paymentRequestWithOrderId(long orderId){
-//        return PaymentRequestRequest.builder()
-//                .userId(1L)
-//                .orderId(orderId)
-//                .orderItemId(1L)
-//                .couponId(1L)
-//                .productId(1L)
-//                .build();
-//    }
-//
-//    public static PaymentRequestRequest paymentRequestWithOrderItemId(long orderItemId){
-//        return PaymentRequestRequest.builder()
-//                .userId(1L)
-//                .orderId(1L)
-//                .orderItemId(orderItemId)
-//                .couponId(1L)
-//                .productId(1L)
-//                .build();
-//    }
-//
-//    public static PaymentRequestRequest paymentRequestWithPaymentId(long paymentId){
-//        return PaymentRequestRequest.builder()
-//                .userId(1L)
-//                .orderId(1L)
-//                .orderItemId(1L)
-//                .couponId(1L)
-//                .productId(1L)
-//                .build();
-//    }
-//
-//    public static PaymentRequestRequest paymentRequestWithProductId(long productId){
-//        return PaymentRequestRequest.builder()
-//                .userId(1L)
-//                .orderId(1L)
-//                .orderItemId(1L)
-//                .couponId(1L)
-//                .productId(productId)
-//                .build();
-//    }
-//
-//    public static ResultActions paymentRequest(MockMvc mockMvc, ObjectMapper objectMapper, PaymentRequestRequest request) throws Exception {
-//
-//        return mockMvc.perform(post(PATH_URL)
-//                                    .content(objectMapper.writeValueAsString(request))
-//                                    .contentType(MediaType.APPLICATION_JSON))
-//                                    .andDo(print());
-//
-//    }
-//
-//}
+package kr.hhplus.be.server.payment.step;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.hhplus.be.server.order.domain.model.Order;
+import kr.hhplus.be.server.payment.domain.model.Payment;
+import kr.hhplus.be.server.payment.domain.model.PaymentStatus;
+import kr.hhplus.be.server.payment.usecase.command.PaymentCommand;
+import kr.hhplus.be.server.payment.usecase.dto.PaymentRequest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+public class PaymentStep {
+    private static String PATH_URL = "/payments";
+
+    public static PaymentCommand defaultPaymentCommand() {
+        return new PaymentCommand(1L, "tossOrderId", "tossPaymentKey", 50_000L);
+    }
+
+    public static Payment defaultPayment() {
+        return Payment.builder()
+                .amount(50_000L)
+                .paymentStatus(PaymentStatus.PENDING)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public static Payment paymentWithOrderId(Order order) {
+        return Payment.builder()
+                .amount(50_000L)
+                .paymentStatus(PaymentStatus.PENDING)
+                .createdAt(LocalDateTime.now())
+                .orderId(order.getId())
+                .build();
+    }
+
+    public static PaymentRequest defaultPaymentRequest(){
+        return PaymentRequest.builder()
+                                .orderId(1L)
+                                .tossPaymentKey("tossPaymentKey")
+                                .tossOrderId("tossOrderId")
+                                .amount(50_000L)
+                                .build();
+    }
+
+    public static PaymentRequest paymentRequestWithOrderId(long orderId) {
+        return PaymentRequest.builder()
+                                .orderId(orderId)
+                                .amount(50_000L)
+                                .tossPaymentKey("tossPaymentKey")
+                                .tossOrderId("tossOrderId")
+                                .build();
+    }
+
+    public static ResultActions paymentRequest(MockMvc mockMvc, ObjectMapper objectMapper, PaymentRequest request) throws Exception {
+        return mockMvc.perform(post(PATH_URL)
+                                    .content(objectMapper.writeValueAsString(request))
+                                    .contentType(MediaType.APPLICATION_JSON))
+                                    .andDo(print());
+    }
+
+    public static Payment paymentWithCreatedAt(LocalDateTime localDateTime) {
+        return Payment.builder()
+                .amount(50_000L)
+                .paymentStatus(PaymentStatus.PENDING)
+                .createdAt(localDateTime)
+                .build();
+    }
+}

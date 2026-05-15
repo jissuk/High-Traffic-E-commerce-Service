3d4d81b59b4740cb9ea4bc680bb23d35c9e91119
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2026-05-16 02:24:04 +0900

    refactor: 유저 쿠폰 레포지토리의 불필요한 메서드 제거

--- src/main/java/kr/hhplus/be/server/coupon/domain/repository/UserCouponRepository.java
+++ src/main/java/kr/hhplus/be/server/coupon/domain/repository/UserCouponRepository.java
@@ -4,7 +4,6 @@ import kr.hhplus.be.server.coupon.domain.model.UserCoupon;
 
 public interface UserCouponRepository {
     UserCoupon findById(long couponId);
-    UserCoupon findByCouponId(long id);
     UserCoupon save(UserCoupon coupon);
 
 }

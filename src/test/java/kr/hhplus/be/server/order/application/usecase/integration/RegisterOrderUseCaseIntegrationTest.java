c20b5e9364a3144f82f478b493e358a16fd4c0bc
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2026-06-01 01:16:19 +0900

    refactor: 주문 등록 로직의 테스트 중 검증 부분 로직 변경

--- src/test/java/kr/hhplus/be/server/order/application/usecase/integration/RegisterOrderUseCaseIntegrationTest.java
+++ src/test/java/kr/hhplus/be/server/order/application/usecase/integration/RegisterOrderUseCaseIntegrationTest.java
@@ -3,16 +3,22 @@ package kr.hhplus.be.server.order.application.usecase.integration;
 import kr.hhplus.be.server.coupon.domain.model.Coupon;
 import kr.hhplus.be.server.coupon.domain.model.CouponStatus;
 import kr.hhplus.be.server.coupon.domain.model.UserCoupon;
+import kr.hhplus.be.server.coupon.fixture.CouponFixture;
 import kr.hhplus.be.server.coupon.infrastructure.jpa.JpaCouponRepository;
 import kr.hhplus.be.server.coupon.infrastructure.jpa.JpaUserCouponRepository;
+import kr.hhplus.be.server.order.domain.model.Order;
+import kr.hhplus.be.server.order.domain.model.OrderItem;
 import kr.hhplus.be.server.order.infrastructure.jpa.JpaOrderItemRepository;
 import kr.hhplus.be.server.order.infrastructure.jpa.JpaOrderRepository;
 import kr.hhplus.be.server.order.application.usecase.RegisterOrderUseCase;
 import kr.hhplus.be.server.order.application.usecase.command.OrderCommand;
+import kr.hhplus.be.server.payment.domain.model.Payment;
 import kr.hhplus.be.server.payment.infrastructure.jpa.JpaPaymentRepository;
 import kr.hhplus.be.server.product.domain.model.Product;
+import kr.hhplus.be.server.product.fixture.ProductFixture;
 import kr.hhplus.be.server.product.infrastructure.jpa.JpaProductRepository;
 import kr.hhplus.be.server.user.domain.model.User;
+import kr.hhplus.be.server.user.fixture.UserFixture;
 import kr.hhplus.be.server.user.infrastructure.jpa.JpaUserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -22,8 +28,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.utility.TestcontainersConfiguration;
-
-import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -57,57 +61,38 @@ public class RegisterOrderUseCaseIntegrationTest {
         @Test
         void 주문() {
             // given
-            long productId = savedProduct.getId();
-            long userId = savedUser.getId();
-            long userCouponId = savedUserCoupon.getId();
-            long quantity = 2L;
-            long point = 3000L;
-            OrderCommand request = OrderCommand.builder()
-                                                .productId(productId)
-                                                .userId(userId)
-                                                .userCouponId(userCouponId)
-                                                .quantity(quantity)
-                                                .point(point)
-                                                .build();
+            OrderCommand command = createOrderCommand();
+            long remainingQuantity = 3L;
 
             // when
-            registerOrderUseCase.execute(request);
+            registerOrderUseCase.execute(command);
 
             // then
-            long createdId = 1L;
+            Product product = productRepository.findById(savedProduct.getId()).orElseThrow();
             assertAll(
-                ()-> assertThat(productRepository.findById(createdId).get().getQuantity())
-                        .as("남은 상품 수량 확인")
-                        .isEqualTo(3L),
-                ()-> assertThat(orderRepository.findById(createdId).get().getId())
-                        .as("주문 생성 확인")
-                        .isEqualTo(createdId),
-                ()-> assertThat(orderItemRepository.findById(createdId).get().getId())
-                        .as("주문 상세 생성 확인")
-                        .isEqualTo(createdId),
-                ()-> assertThat(paymentRepository.findById(createdId).get().getId())
-                        .as("결제 생성 확인")
-                        .isEqualTo(createdId)
+                    () -> assertThat(product.getQuantity())
+                            .isEqualTo(remainingQuantity),
+
+                    () -> assertThat(orderRepository.count())
+                            .isEqualTo(1),
+
+                    () -> assertThat(orderItemRepository.count())
+                            .isEqualTo(1),
+
+                    () -> assertThat(paymentRepository.count())
+                            .isEqualTo(1)
             );
         }
     }
 
     private void initTestDBData(){
-        User user = User.builder()
-                        .point(10_000L)
-                        .build();
+        Product product = ProductFixture.create(); // quantity : 5
+        savedProduct = productRepository.save(product);
+
+        User user = UserFixture.create();
         savedUser = userRepository.save(user);
 
-        long couponDiscount = 3000L;
-        long couponQuantity = 500L;
-        String couponDescription = "여름 특별 할인 쿠폰";
-        LocalDateTime expiration = LocalDateTime.now().plusMonths(3);
-        Coupon coupon = Coupon.builder()
-                                .discount(couponDiscount)
-                                .quantity(couponQuantity)
-                                .description(couponDescription)
-                                .expiredAt(expiration)
-                                .build();
+        Coupon coupon = CouponFixture.create();
         savedCoupon = couponRepository.save(coupon);
 
         UserCoupon userCoupon = UserCoupon.builder()
@@ -116,12 +101,20 @@ public class RegisterOrderUseCaseIntegrationTest {
                                             .couponId(savedCoupon.getId())
                                             .build();
         savedUserCoupon = userCouponRepository.save(userCoupon);
+    }
 
-        Product product = Product.builder()
-                .productName("기본 상품")
-                .price(2000L)
-                .quantity(5L)
+    private OrderCommand createOrderCommand(){
+        long productId = savedProduct.getId();
+        long userId = savedUser.getId();
+        long userCouponId = savedUserCoupon.getId();
+        long quantity = 2L;
+        long point = 3000L;
+        return OrderCommand.builder()
+                .productId(productId)
+                .userId(userId)
+                .userCouponId(userCouponId)
+                .quantity(quantity)
+                .point(point)
                 .build();
-        savedProduct = productRepository.save(product);
     }
 }

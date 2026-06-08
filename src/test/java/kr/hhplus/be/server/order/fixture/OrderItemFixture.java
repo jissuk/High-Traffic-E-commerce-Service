52aaa1dd2890b2cafe349a8911682e681590696e
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2026-06-09 00:31:22 +0900

    refactor: Order도메인의 Fixture 클래스에 Test Data Build 패턴 적용

--- src/test/java/kr/hhplus/be/server/order/fixture/OrderItemFixture.java
+++ src/test/java/kr/hhplus/be/server/order/fixture/OrderItemFixture.java
@@ -4,10 +4,32 @@ import kr.hhplus.be.server.order.domain.model.OrderItem;
 
 public class OrderItemFixture {
 
-    public static OrderItem withProductIdAndQuantity(long productId, long quantity) {
+    private Long quantity;
+    private Long productId;
+
+    private OrderItemFixture() {
+        this.quantity = 999L;
+        this.productId = 999L;
+    }
+
+    public static OrderItemFixture builder(){
+        return new OrderItemFixture();
+    }
+
+    public OrderItemFixture quantity(Long quantity) {
+        this.quantity = quantity;
+        return this;
+    }
+
+    public OrderItemFixture productId(Long productId) {
+        this.productId = productId;
+        return this;
+    }
+
+    public OrderItem build(){
         return OrderItem.builder()
-                .productId(productId)
                 .quantity(quantity)
+                .productId(productId)
                 .build();
     }
 }

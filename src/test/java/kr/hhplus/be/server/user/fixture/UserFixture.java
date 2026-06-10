a56c586ee4dd315e44debcab768a55448bb3721d
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2026-06-11 00:32:03 +0900

    refactor: User도메인의 Fixture 클래스에 Test Data Build 패턴 적용

--- src/test/java/kr/hhplus/be/server/user/fixture/UserFixture.java
+++ src/test/java/kr/hhplus/be/server/user/fixture/UserFixture.java
@@ -3,9 +3,26 @@ package kr.hhplus.be.server.user.fixture;
 import kr.hhplus.be.server.user.domain.model.User;
 
 public class UserFixture {
-    public static User create() {
+
+    private Long point;
+
+    private UserFixture(){
+        this.point = 40_000L;
+    }
+
+    public static UserFixture builder(){
+        return new UserFixture();
+    }
+
+    public User build(){
         return User.builder()
-                .point(40_000L)
+                .point(point)
                 .build();
     }
+
+//    public static User create() {
+//        return User.builder()
+//                .point(40_000L)
+//                .build();
+//    }
 }

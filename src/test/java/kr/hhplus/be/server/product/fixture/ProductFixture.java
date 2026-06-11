eefa4af0f9b04e9c61a67adda5dd4297289f6843
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2026-06-12 00:32:34 +0900

    refactor: Product 도메인의 Fixture 클래스에 Test Data Build 패턴 적용

--- src/test/java/kr/hhplus/be/server/product/fixture/ProductFixture.java
+++ src/test/java/kr/hhplus/be/server/product/fixture/ProductFixture.java
@@ -4,20 +4,50 @@ import kr.hhplus.be.server.product.domain.model.Product;
 
 public class ProductFixture {
 
-    public static Product create(){
+    private Long id;
+    private String productName;
+    private Long price;
+    private Long quantity;
+
+    private ProductFixture(){
+        this.id = 0L;
+        this.productName = "기본 상품";
+        this.price = 2000L;
+        this.quantity = 5L;
+    }
+
+    public static ProductFixture builder(){
+        return new ProductFixture();
+    }
+
+    public ProductFixture id(Long id){
+        this.id = id;
+        return this;
+    }
+
+    public Product build(){
         return Product.builder()
-                .productName("기본 상품")
-                .price(2000L)
-                .quantity(5L)
+                .id(id)
+                .productName(productName)
+                .price(price)
+                .quantity(quantity)
                 .build();
     }
 
-    public static Product withProductId(long id){
-        return Product.builder()
-                .id(id)
-                .productName("기본 상품")
-                .price(2000L)
-                .quantity(5L)
-                .build();
-    }
+//    public static Product create(){
+//        return Product.builder()
+//                .productName("기본 상품")
+//                .price(2000L)
+//                .quantity(5L)
+//                .build();
+//    }
+
+//    public static Product withId(long id){
+//        return Product.builder()
+//                .id(id)
+//                .productName("기본 상품")
+//                .price(2000L)
+//                .quantity(5L)
+//                .build();
+//    }
 }

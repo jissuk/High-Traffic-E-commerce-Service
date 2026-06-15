93ac35f8d1ed923eb8030734af8ad618b7f7064e
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2026-06-15 10:11:45 +0900

    refactor: 쿠폰 통합 테스트 로직을 통합, 동시성 테스트로 분리

--- src/test/java/kr/hhplus/be/server/coupon/application/usecase/concurrency/IssueCouponUseCaseConcurrencyTest.java
+++ src/test/java/kr/hhplus/be/server/coupon/application/usecase/concurrency/IssueCouponUseCaseConcurrencyTest.java
@@ -1,40 +1,39 @@
-package kr.hhplus.be.server.coupon.application.usecase.integration;
+package kr.hhplus.be.server.coupon.application.usecase.concurrency;
 
-import kr.hhplus.be.server.common.outbox.scheduler.OutboxRelayScheduler;
+import kr.hhplus.be.server.coupon.application.command.UserCouponCommand;
+import kr.hhplus.be.server.coupon.application.usecase.IssueCouponUseCase;
 import kr.hhplus.be.server.coupon.domain.model.Coupon;
-import kr.hhplus.be.server.coupon.exception.DuplicateCouponIssueException;
 import kr.hhplus.be.server.coupon.infrastructure.jpa.JpaCouponRepository;
 import kr.hhplus.be.server.coupon.infrastructure.jpa.JpaUserCouponRepository;
-import kr.hhplus.be.server.coupon.application.usecase.IssueCouponUseCase;
-import kr.hhplus.be.server.coupon.application.command.UserCouponCommand;
 import kr.hhplus.be.server.coupon.infrastructure.redis.CouponRedisKey;
 import kr.hhplus.be.server.user.domain.model.User;
 import kr.hhplus.be.server.user.infrastructure.jpa.JpaUserRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.testcontainers.utility.TestcontainersConfiguration;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicLong;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-@Slf4j
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @Import(TestcontainersConfiguration.class)
-@DisplayName("쿠폰 관련 통합 테스트")
-public class IssueCouponUseCaseIntegrationTest {
+@DisplayName("쿠폰 관련 동시성 테스트")
+public class IssueCouponUseCaseConcurrencyTest {
 
-    @Autowired private IssueCouponUseCase issueCouponUseCase;
-    @Autowired private OutboxRelayScheduler outboxRelayScheduler;
+    @Autowired
+    private IssueCouponUseCase issueCouponUseCase;
     @Autowired private RedisTemplate<String, Long> redis;
     @Autowired private JpaUserRepository userRepository;
     @Autowired private JpaCouponRepository couponRepository;
@@ -51,52 +50,65 @@ public class IssueCouponUseCaseIntegrationTest {
         initTestRedisData();
     }
 
-    @Nested
-    @DisplayName("성공 케이스")
-    class success {
+    @Test
+    @DisplayName("동시에 10건의 실시간쿠폰발급 요청을 보낼 경우 모두 성공")
+    void 실시간쿠폰발급_분산락_동시성() throws Exception {
+        // given
+        long couponId = savedCoupon.getId();
+        int threadCount = 10;
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(threadCount);
 
-        @Test
-        @DisplayName("실시간 쿠폰 발급 Kafka 비동기 처리")
-        void 쿠폰발급요청시_비동기적으로_유저쿠폰이_생성된다(){
-            // given
-            long userId = savedUser.get(0).getId();
-            long couponId = savedCoupon.getId();
-            UserCouponCommand command = new UserCouponCommand(userId, couponId);
-            String quantityKey = CouponRedisKey.getQuantityKey(savedCoupon.getId());
-            long remainingCoupons = 9L;
+        List<Future<Void>> futures = new ArrayList<>();
 
-            // when
-            issueCouponUseCase.execute(command);
-            outboxRelayScheduler.relay();
-
-            // then
-            Long couponQuantity = redis.opsForValue().get(quantityKey);
-
-            await()
-                    .atMost(Duration.ofSeconds(5))
-                    .untilAsserted(() -> {
-                        assertThat(couponQuantity).isEqualTo(remainingCoupons);
-                        assertThat(userCouponRepository.findByCouponIdAndUserId(couponId, userId));
-                    });
+        for (int i = 0; i < threadCount; i++) {
+            int userIndex = i;
+            futures.add(executor.submit(() -> {
+                // when
+                try {
+                    long userId = savedUser.get(userIndex).getId();
+                    UserCouponCommand command = new UserCouponCommand(userId, couponId);
+                    issueCouponUseCase.execute(command);
+                    return null;
+                } finally {
+                    latch.countDown();
+                }
+            }));
         }
-    }
+        latch.await();
+        executor.shutdown();
 
-    @Nested
-    @DisplayName("실패 케이스")
-    class fail {
-        @Test
-        @DisplayName("실시간 쿠폰 발급 동일 유저 발급")
-        void 실시간쿠폰발급_동일유저발급(){
-            // given
-            long userId = savedUser.get(0).getId();
-            long couponId = savedCoupon.getId();
-            UserCouponCommand request = new UserCouponCommand(userId, couponId);
-            issueCouponUseCase.execute(request);
+        AtomicLong successCount = new AtomicLong();
+        AtomicLong failureCount = new AtomicLong();
 
-            // when & then
-            assertThatThrownBy(() -> issueCouponUseCase.execute(request))
-                    .isInstanceOf(DuplicateCouponIssueException.class);
+        for (Future<Void> future : futures) {
+            try {
+                future.get();
+                successCount.getAndIncrement();
+            } catch (ExecutionException e) {
+                if (e.getCause() instanceof ObjectOptimisticLockingFailureException) {
+                    failureCount.getAndIncrement();
+                } else{
+                    e.printStackTrace();
+                }
+            }
         }
+
+        // then
+        String quantityKey = CouponRedisKey.getQuantityKey(couponId);
+        Long couponQuantity = redis.opsForValue().get(quantityKey);
+
+        assertAll(
+                ()-> assertThat(couponQuantity)
+                        .as("잔여 쿠폰 개수 확인")
+                        .isEqualTo(0L),
+                ()-> assertThat(successCount.get())
+                        .as("성공한 요청 수")
+                        .isEqualTo(10),
+                ()-> assertThat(failureCount.get())
+                        .as("실패한 요청 수")
+                        .isEqualTo(0)
+        );
     }
 
     private void clearTestDBData(){
